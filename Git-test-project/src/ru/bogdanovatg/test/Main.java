56a91545528b60a5c6e7d222b62da09ec84293ba
bogdanovatg <bogda15@rambler.ru>
2015-05-23 11:34:47 +0600

    add 1

--- Git-test-project/src/ru/bogdanovatg/test/Main.java
+++ Git-test-project/src/ru/bogdanovatg/test/Main.java
@@ -2,7 +2,7 @@ package ru.bogdanovatg.test;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub!!!");
+		System.out.println("Hello from Git Hub!!!!");
 	}	
 	
 }
